b1d59317c4e0692c8bb92d1f9fe4c127626b8e5d
Lorenzo Mirabella <lorenzo.mirabella001@studenti.uniparthenope.it>
2024-01-22 16:40:30 +0100

    Primo commit

---
+++ cmake-build-debug/CMakeFiles/Starship.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Starship.dir/main.c.o"
+  "CMakeFiles/Starship.dir/main.c.o.d"
+  "Starship"
+  "Starship.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Starship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
